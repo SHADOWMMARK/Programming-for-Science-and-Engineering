6b7ed02100392a209b335201b92a18cdc5089f9a
Mengyao Huang <huangmy@umich.edu>
2019-03-23 17:23:36 -0400

    support c++ files and header files

---
+++ Image modification/isCinValid.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "isCinValid.h"
+using namespace std;
+
+// check whether the choice is valid
+bool isCinValid(int inputValue, int lower, int upper) {
+    if (inputValue < lower) {
+        return false;
+    }
+    if (inputValue > upper) {
+        return false;
+    }
+    return true;
+}
---
+++ Image modification/mainMenuOption.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "mainMenuOption.h"
+#include "Constant.h"
+using namespace std;
+
+// print main menu and return input choice
+// return should be 1-5
+int mainMenuOption() {
+    int choice;
+    bool validInputFound = false;
+    cout << "1. Annotate image with rectangle" << endl;
+    cout << "2. Annotate image with pattern from file" << endl;
+    cout << "3. Insert another image" << endl;
+    cout << "4. Write out current image" << endl;
+    cout << "5. Exit the program" << endl;
+    cout << "Enter int for main menu choice : ";
+    while (!validInputFound) {
+        cin >> choice;
+        if (cin.fail() || (choice > CHOICE_MAX) || (choice < CHOICE_MIN)) {
+            cin.clear();
+            cin.ignore(200, '\n');
+            cout << "Please print valid choice number from 1-5!" << endl;
+            cout << "Enter int for main menu choice: ";
+        }
+        else {
+            validInputFound = true;
+        }
+    }
+    return choice;
+}
